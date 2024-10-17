889f44fa619bfc79b8fe637db0144ae98fb14188
HG <hirdyanshu29@gmail.com>
2024-10-17 14:35:16 +0530

    HTML

---
+++ hitesh.js
@@ -0,0 +1,7 @@
+// Import everything using require
+const check = require("./json.js");
+
+console.log("Add:",check.add(5,3));
+console.log("Subtract:",check.subtract(5,3));
+console.log("PI",check.PI);
+console.log("Multiply:",check.multiply(5,3));
